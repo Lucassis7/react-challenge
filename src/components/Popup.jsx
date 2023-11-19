71d9b988d3fd72862b6de2919b457f4539791b7b
Lucassis7 <lucassis7@outlook.com>
2023-11-19 02:57:59 -0300

    feat: create Popup component

---
+++ src/components/Popup.jsx
@@ -0,0 +1,29 @@
+function Popup({ data, onClose, lastSeenTools }) {
+  return (
+    <div className="popup">
+      <div className="popup-content">
+        <div className="popup-first-frame">
+          <img src={data.icon} alt="" />
+          <div className="popup-infos-container">
+            <h2 className="popup-title-text">{data.name}</h2>
+            <a className="popup-title-link" href={data.link} target="_blank" rel="noreferrer">ACESSAR</a>
+          </div>
+        </div>
+        <div className="popup-last-tools">
+          <h3>ÚLTIMAS FERRAMENTAS VISUALIZADAS</h3>
+          <div className="last-tools-container">
+            {lastSeenTools.map((tool, index) => (
+              <div key={index}>
+                <img src={tool.icon} alt="" width="50px" height="50px"/>
+                <p>{tool.name}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+        <button onClick={onClose}>Fechar</button>
+      </div>
+    </div>
+  );
+}
+
+export default Popup;
